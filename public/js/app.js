13148a43a6c80a67177f96e9d4e547d8096bc774
Jonas <jonaspaq@gmail.com>
2020-01-25 12:09:36 +0800

    Updated a security issue

--- public/js/app.js
+++ public/js/app.js
@@ -60365,8 +60365,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Nexseed\Desktop\PersonalFolder\laravelecho\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Nexseed\Desktop\PersonalFolder\laravelecho\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Paquibot\Desktop\junjayfolder\myprojects\laravelecho\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Paquibot\Desktop\junjayfolder\myprojects\laravelecho\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
